f1fc33284bb12d62a7e6537a658ae5e5e75b1419
lliuaotian <liuaotianmsj@gmail.com>
2023-09-11 03:35:26 -0700

    增加了切换vim中tab的快捷方式

--- lua/liuaotian/remap.lua
+++ lua/liuaotian/remap.lua
@@ -3,3 +3,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>sg", vim.cmd.UndotreeToggle)
 -- 设置leader+n打开文件树
 vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeToggle)
+
+-- 切换vim的tab到下一个
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabn)
+-- 切换vim的tab到上一个
+vim.keymap.set("n", "<leader>tp", vim.cmd.tabp)
+-- tab之间循环切换
+vim.keymap.set("n", "<leader>tt", vim.cmd.tabp)
