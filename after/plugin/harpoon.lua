2496066e1e126e021778dc0679062a5ebb99f646
lliuaotian <liuaotianmsj@gmail.com>
2023-09-11 03:56:19 -0700

    将暂时用不到，记不住的快捷键先注释起来，避免和其他快捷键冲突

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,8 +6,8 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 -- 列出文件
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
--- 切换到第1、2、3、4file
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+-- 切换到第1、2、3、4file 记不住的快捷键索性先挂起来，使用quick_menu就行
+-- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+-- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+-- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+-- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
