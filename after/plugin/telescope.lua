eca6ad4bdbdb77a8d79c2389f5af3fdc11d9f1b1
lliuaotian <liuaotianmsj@gmail.com>
2023-09-11 03:33:03 -0700

    删除非必要代码，增加窗口说明

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,26 @@
+-- 设置快捷键
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 -- 根据文件内容查找文件
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 -- 查找git仓库中的文件
 vim.keymap.set('n', '<leader>git', builtin.git_files, {})
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+-- help doc查看当前帮助文档
+vim.keymap.set('n', '<leader>hd', builtin.help_tags, {})
+-- 当前工作的缓冲区
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+-- 设置出现的窗口宽高
+require('telescope').setup{
+  defaults = {
+    -- 修改显示的窗口布局 通过help telescope中layout_strategy可以查看
+    -- 如果列的数量低于120
+    -- 不会显示右边的文件内容，layout_strategy的默认内容就是horizontal
+    layout_strategy = 'horizontal',
+    layout_config = {
+      -- height = 0.7,
+      -- 这一行设置 不管当前窗口多少列都显示preview，但是这样肯定显示的效果不好
+      preview_cutoff = 0,
+    },
+  }
+}
