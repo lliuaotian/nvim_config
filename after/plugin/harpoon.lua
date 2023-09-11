2a87eb05370056d565239c100aee69de41a27a51
lliuaotian <liuaotianmsj@gmail.com>
2023-09-11 06:24:33 -0700

    修改两个快捷键 更改为缩写，更好理解

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,10 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
--- 添加文件到buf
-vim.keymap.set("n", "<leader>a", mark.add_file)
--- 列出文件
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+-- 添加文件到buf 更改快捷键为harpoon mark的缩写
+vim.keymap.set("n", "<leader>hm", mark.add_file)
+-- 列出文件 更改快捷键ctrl-e改为harpoon buf menu
+vim.keymap.set("n", "<leader>hbm", ui.toggle_quick_menu)
 
 -- 切换到第1、2、3、4file 记不住的快捷键索性先挂起来，使用quick_menu就行
 -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
